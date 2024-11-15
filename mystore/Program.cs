0532dc84716a7af853e917d96461dfaece3c2629
Albiona Berisha <ab47853@ubt-uni.net>
2024-11-15 16:00:19 +0200

    finished

--- mystore/Program.cs
+++ mystore/Program.cs
@@ -1,75 +1,31 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using mystore.Models;
 using mystore.Services;
+using Microsoft.AspNetCore.Identity;
+using mystore.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Shto shërbimet në container.
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseSqlServer(connectionString);
+	var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+	options.UseSqlServer(connectionString);
 });
 
-// Shto dhe konfiguro Identitetin me rolin për administratorin
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddRoles<IdentityRole>() // Shto mbështetje për role
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-// Konfiguro cookie për të ridrejtuar në faqen e hyrjes
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.LoginPath = "/Identity/Account/Login";
-    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-});
-
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
 
-// Metoda për të krijuar përdoruesin dhe rolin e administratorit
-async Task CreateAdminUserAsync(IServiceProvider serviceProvider)
-{
-    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-    string adminRole = "Admin";
-    string adminEmail = "admin@shembull.com";
-    string adminPassword = "Admin123!";
-
-    // Krijo rolin Admin nëse nuk ekziston
-    if (!await roleManager.RoleExistsAsync(adminRole))
-    {
-        await roleManager.CreateAsync(new IdentityRole(adminRole));
-    }
-
-    // Krijo përdoruesin administrator nëse nuk ekziston
-    var adminUser = await userManager.FindByEmailAsync(adminEmail);
-    if (adminUser == null)
-    {
-        adminUser = new ApplicationUser { UserName = adminEmail, Email = adminEmail };
-        await userManager.CreateAsync(adminUser, adminPassword);
-        await userManager.AddToRoleAsync(adminUser, adminRole);
-    }
-}
-
-// Thirr metodën për të krijuar administratorin
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    await CreateAdminUserAsync(services);
-}
-
-// Konfiguro pipeline të kërkesave HTTP
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+	app.UseExceptionHandler("/Error");
 }
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication(); // Shto Autentifikimin
 app.UseAuthorization();
 
 app.MapRazorPages();
